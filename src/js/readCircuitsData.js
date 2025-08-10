f4f5205e37e45cb8f239000bac928ea08ded8a1b
Janick Sturm <sturmjanick@gmail.com>
2025-08-10 21:19:33 +0200

    display circuits data

--- src/js/readCircuitsData.js
+++ src/js/readCircuitsData.js
@@ -1,7 +1,29 @@
 fetch('https://api.jolpi.ca/ergast/f1/current.json')
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      displayData(data);
     })
     .catch(error => console.error('Error fetching data:', error)
     );
+
+
+function displayData(data) {
+  const container = document.getElementById('race-container');
+      
+  data.MRData.RaceTable.Races.forEach(race => {
+    const raceBox = document.createElement('div');
+    raceBox.className = 'race-box';
+    
+    raceBox.innerHTML = `
+      <img class="race-image" src="" alt="Picture of ${race.Circuit.circuitName}" />
+      <div class="race-info">
+        <h2>${race.raceName}</h2>
+        <p><strong>Date:</strong> ${race.date}</p>
+        <p><strong>Name:</strong> ${race.Circuit.circuitName}</p>
+        <p><strong>Location:</strong> ${race.Circuit.Location.locality}, ${race.Circuit.Location.country}</p>
+        <a href="${race.Circuit.url}">See more on Wikipedia</a>
+      </div>
+    `;
+    container.appendChild(raceBox);
+  });
+}
