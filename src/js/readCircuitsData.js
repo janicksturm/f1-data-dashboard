4a082fec5e88f268edb5ec31b052084b12196af9
Janick Sturm <sturmjanick@gmail.com>
2025-08-10 23:30:57 +0200

    add pictures for each circuit

--- src/js/readCircuitsData.js
+++ src/js/readCircuitsData.js
@@ -1,21 +1,30 @@
-fetch('https://api.jolpi.ca/ergast/f1/current.json')
-    .then(response => response.json())
-    .then(data => {
-      displayData(data);
-    })
-    .catch(error => console.error('Error fetching data:', error)
-    );
+let circuitImages = {};
 
+fetch('https://api.jolpi.ca/ergast/f1/current.json')
+  .then(res => res.json())
+  .then(data => {
+    fetch('../images/circuitImages.json')
+      .then(res => res.json())
+      .then(images => {
+        circuitImages = images;
+        displayData(data);
+      });
+  }
+);
 
 function displayData(data) {
   const container = document.getElementById('race-container');
       
   data.MRData.RaceTable.Races.forEach(race => {
     const raceBox = document.createElement('div');
-    raceBox.className = 'race-box';
+    raceBox.className = 'race-box'; 
+
+    const imageUrl = circuitImages[race.Circuit.circuitName].imageUrl;
     
     raceBox.innerHTML = `
-      <img class="race-image" src="" alt="Picture of ${race.Circuit.circuitName}" />
+      <a href="${imageUrl}">
+        <img class="race-image" src="${imageUrl}" alt="Picture of ${race.Circuit.circuitName}" />
+      </a>
       <div class="race-info">
         <h2>${race.raceName}</h2>
         <p><strong>Date:</strong> ${race.date}</p>
